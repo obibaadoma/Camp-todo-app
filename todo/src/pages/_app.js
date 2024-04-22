0e88f87831ffa4d6e79b8a78cd1e6f9c1fff39f6
obibaadoma <akrasikonadu@yahoo.com>
2024-04-22 20:54:19 +0000

    added readme

--- todo/src/pages/_app.js
+++ todo/src/pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+// src/app/page.js
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import React from "react";
+import Form from "@/components/Form";
+import Header from "@/components/Header";
+import TODOHero from "@/components/TODOHero";
+import TODOList from "@/components/TODOList";
+function Home() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <TODOHero todos_completed={0} total_todos={0} />
+      <Form />
+      <TODOList todos={[]} />
+    </div>
+  );
 }
+export default Home;
